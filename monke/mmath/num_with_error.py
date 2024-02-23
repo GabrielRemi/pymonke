8acf3c072f2c394730746be8beffa929516f01e3
Gabriel Remiszewski <Gabriel.Remiszewski@outlook.de>
2024-02-23 17:10:51 +0100

    NumWithError display_separate + no arrays

--- monke/mmath/num_with_error.py
+++ monke/mmath/num_with_error.py
@@ -4,7 +4,7 @@ from typing import Tuple, Any, List
 from rounding import roundup_two_significant_digits
 
 
-def _round_values(x: float, x_error: float) -> Tuple[float, float]:
+def _round_values(x: float, x_error: float) -> Tuple[float, float, float]:
     """rounds up to 2 significant digits. functions only takes float values as arguments"""
     if not (isinstance(x, float) and isinstance(x_error, float)):
         raise ValueError("both x and x_error must be floating point numbers")
@@ -20,28 +20,17 @@ def _round_values(x: float, x_error: float) -> Tuple[float, float]:
 
     x = round(x, rounding_decimal)
 
-    return x, x_error
+    return x, x_error, int(exponent)
 
 
 class NumWithError:
-    """inputs two numbers and rounds them appropriately, treating __x_error as an uncertainty.
-    __x and __x_error could also be array_like objects."""
-
-    def __init__(self, x, x_error):
-        if isinstance(x, (float, int)):
+    """inputs two numbers and rounds them appropriately, treating __x_error as an uncertainty."""
+    def __init__(self, x: float | int,  x_error: float | int) -> None:
+        if isinstance(x, (float, int)) and isinstance(x_error, (float, int)):
             x, x_error = float(x), float(x_error)
-            self.__x, self.__x_error = _round_values(x, x_error)
+            self.__x, self.__x_error, self.__exponent = _round_values(x, x_error)
         else:
-            x = [float(i) for i in list(x)]
-            x_error = [float(i) for i in list(x_error)]
-            if len(x) != len(x_error):
-                raise ValueError("Length of x and x_error have to be the same")
-
-            self.__x, self.__x_error = [], []
-            for num, error in zip(x, x_error):
-                num, error = _round_values(num, error)
-                self.__x.append(num)
-                self.__x_error.append(error)
+            raise TypeError("x and x_error must be numbers")
 
     @property
     def x(self):
@@ -51,8 +40,19 @@ class NumWithError:
     def x_error(self):
         return self.__x_error
 
-    def get_values(self) -> Tuple[float, float] | Tuple[List[float], List[float]]:
+    def get_values(self) -> Tuple[float, float]:
         return self.__x, self.__x_error
+
+    def display_separate(self) -> str:
+        """outputs a string with the number and uncertainty separated by '+-' in a <num> block.
+        This functionality is meant to be used for work in latex with the siunitx package."""
+
+        precision = ''
+        format_str = "{:}"
+        if self.__exponent < 0:
+            precision = str(-self.__exponent + 1)
+            format_str = f"{{:.{precision}f}}"
+        return format_str.format(self.x) + " +- " + format_str.format(self.x_error)
 
     def __eq__(self, other):
         return self.__x == other.__x and self.__x_error == other.__x_error
--- monke/mmath/tests/num_with_error_test.py
+++ monke/mmath/tests/num_with_error_test.py
@@ -5,7 +5,7 @@ from monke.mmath import NumWithError
 
 class MyTestCase(unittest.TestCase):
     def test_num_with_error_eq(self):
-        self.assertEqual(NumWithError(2.234, 0.123), NumWithError(2.23, 0.13))  # add assertion here
+        self.assertEqual(NumWithError(2.234, 0.123), NumWithError(2.23, 0.13))
         self.assertEqual(NumWithError(2.234, 0.14234), NumWithError(2.23, 0.15))
         self.assertEqual(NumWithError(2, 0.14234), NumWithError(2.00, 0.15))
         self.assertEqual(NumWithError(2, 0.16234), NumWithError(2.00, 0.17))
@@ -36,20 +36,42 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(NumWithError(0.8234, 110.0002).get_values(), (0, 120))
         self.assertEqual(NumWithError(1423.12341287, 103.814123).get_values(), (1420, 110))
 
-    def test_num_with_error_arrays(self):
+    def test_num_with_error_display_separate(self):
+
+        self.assertEqual(NumWithError(2.234, 0.14234).display_separate(), "2.23 +- 0.15")
+        self.assertEqual(NumWithError(2, 0.14234).display_separate(), "2.00 +- 0.15")
+        self.assertEqual(NumWithError(2, 0.16234).display_separate(), "2.00 +- 0.17")
+        self.assertEqual(NumWithError(2.234123, 0.16234).display_separate(), "2.23 +- 0.17")
+        self.assertEqual(NumWithError(2.234123, 0.19934).display_separate(), "2.20 +- 0.20")
+        self.assertEqual(NumWithError(0.9, 0.99).display_separate(), "1.0 +- 1.0")
+        self.assertEqual(NumWithError(10.8567, 0.99).display_separate(), "11.0 +- 1.0")
+        self.assertEqual(NumWithError(114.123, 10.89123).display_separate(), "114.0 +- 11.0")
+        self.assertEqual(NumWithError(0.8234, 10.914123).display_separate(), "1.0 +- 11.0")
+        self.assertEqual(NumWithError(0.08234, 10.914123).display_separate(), "0.0 +- 11.0")
+        self.assertEqual(NumWithError(0.8234, 103.814123).display_separate(), "0.0 +- 110.0")
+        self.assertEqual(NumWithError(0.8234, 110.0002).display_separate(), "0.0 +- 120.0")
+        self.assertEqual(NumWithError(1423.12341287, 103.814123).display_separate(), "1420.0 +- 110.0")
         self.assertEqual(
-            NumWithError([2.234, 2, 2], [0.14234, 0.14234, 0.16234]).get_values(),
-            ([2.23, 2.00, 2.00], [0.15, 0.15, 0.17]))
+            NumWithError(0.000_000_000_712_312, 0.000_000_000_051_512_123).display_separate(),
+            "0.000000000710 +- 0.000000000060")
 
     def test_num_with_error_raise(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(TypeError):
             NumWithError(23, "not a number")
+        with self.assertRaises(TypeError):
             NumWithError("not a number", 23)
+        with self.assertRaises(TypeError):
             NumWithError([2.234, 2, "asd"], [0.14234, 0, 123])
+        with self.assertRaises(TypeError):
             NumWithError([2, 3, 4], [0.14234, 0])
-            NumWithError([2, 3], [0.14234, 0, 0.123])
+        with self.assertRaises(ValueError):
+            NumWithError(2, 0)
+        with self.assertRaises(ValueError):
             NumWithError(2, -0.123)
+        with self.assertRaises(ValueError):
             NumWithError(-1.23, 0)
+        with self.assertRaises(TypeError):
+            NumWithError([2, 3], [-0.14234, 0, 0.123])
 
 
 if __name__ == '__main__':
